4a33255eeab8f6182c6638aaf2776b4a8d74495a
trripy <trripy01@gmail.com>
2021-07-26 07:37:42 +0100

    Added Working transmit Function

--- ringbufferk.c
+++ ringbufferk.c
@@ -4,5 +4,73 @@
  *  Created on: Jun 15, 2021
  *      Author: kanka
  */
+#include "ringbuffer.h"
 
+void Serial_begin(UART_HandleTypeDef *uart){
+RxBuffer.uart=uart;
+TxBuffer.uart=uart;
+}
 
+void __push(RingBuffer *buff,uint8_t a){
+uint8_t i=(unsigned int)(buff->_head + 1) % UART_BUFFER_SIZE;
+if(i!=buff->_tail){
+	buff->_buffer[buff->_head]=a;
+	buff->_head=i;
+}
+else{
+//do nothing
+}
+}
+
+uint8_t __pop(RingBuffer *buff){
+	if(buff->_tail==buff->_head)
+		{
+			return 0;
+		}
+	else{
+		uint8_t __data=buff->_buffer[buff->_tail];
+		buff->_tail=(unsigned int)(buff->_tail + 1) % UART_BUFFER_SIZE;
+		return __data;
+	}
+}
+
+uint8_t Serial_available(RingBuffer *buff){
+	 return ((unsigned int)(UART_BUFFER_SIZE + buff->_head - buff->_tail)) % UART_BUFFER_SIZE;
+}
+
+void Serial_write(uint8_t c){
+__push(&TxBuffer,c);
+__HAL_UART_ENABLE_IT(TxBuffer.uart,UART_IT_TXE);
+}
+void Serial_println(const char *datatosend){
+	while(*datatosend) Serial_write((uint8_t)*datatosend++);
+	Serial_write(0x00); // NULL
+	Serial_write(0x01); // CTRL+A Start of Header(SOH)
+	Serial_write(0x03); // CTRL+C End of Text (ETX)
+	Serial_write(0x04); // CTRL+D End of Transmission (EOT)
+	Serial_write(0x05); // CTRL+E Enquiry (SOH)
+	Serial_write(0x06); // CTRL+F Acknowledgement (ETX)
+	Serial_write(0x07); // CTRL+G BEL
+	Serial_write(0x0A); // New Line/LineFeed
+	Serial_write(0x0D); // Carriage Feed
+	Serial_write(0x18); // Cancel
+	Serial_write(0x1A); // Substitute
+
+}
+void UartHandler(UART_HandleTypeDef * uart){
+
+	if(uart->Instance->ISR & USART_ISR_RXNE){
+		_ReceivedData=(uint8_t)uart->Instance->RDR;
+		__push(&RxBuffer,_ReceivedData);
+	}
+	if(uart->Instance->ISR & USART_ISR_TXE){
+		uint8_t _TransmitData=__pop(&TxBuffer);
+		if(_TransmitData!=0){
+//			uart->Instance->ISR;
+			uart->Instance->TDR=_TransmitData;
+		}
+		else{
+			__HAL_UART_DISABLE_IT(TxBuffer.uart,UART_IT_TXE);
+		}
+	}
+}
--- ringbufferk.h
+++ ringbufferk.h
@@ -9,33 +9,44 @@
 #define INC_RINGBUFFERK_H_
 
 /* Inlcude your respective STM32 HAL Library*/
-#include "stm32f4xx_hal.h"
+#include "stm32f0xx_hal.h"
 
 // Define Here what should be the buffer size of the ring buffer
-#define UART_BUFFER_SIZE 512
+#define UART_BUFFER_SIZE 64
 
+#if (UART_BUFFER_SIZE > 256)
+typedef uint16_t rxbuffer_Index_Type;
+typedef uint16_t txbuffer_Index_Type;
+#else
+typedef uint8_t rxbuffer_Index_Type;
+typedef uint8_t txbuffer_Index_Type;
+#endif
 
 typedef struct{
-	uint8_t _buffer[UART_BUFFER_SIZE];
+	volatile uint8_t _buffer[UART_BUFFER_SIZE];
 	volatile unsigned int _head;
 	volatile unsigned int _tail;
+	UART_HandleTypeDef *uart;
 }RingBuffer;
+uint8_t _ReceivedData;
+RingBuffer RxBuffer,TxBuffer;
+
+
+void __push(RingBuffer *buff,uint8_t a);
+uint8_t __pop(RingBuffer *buff);
+
+
+
 
 void Serial_begin();
-boolean Serial_available();
-boolean Serial_flush();
+uint8_t Serial_available();
+uint8_t Serial_flush();
 uint8_t Serial_read();
-void Serial_write(uint8_t _char);
+void Serial_write(uint8_t c);
 void Serial_print(const char *s);
 void Serial_println(const char *s);
 void Serial_find();
 void Serial_findUntil();
 void Serial_peek();
-
-#ifdef UART_BUFFER_SIZE > 255
-uint8_t Serial_readBytes();
-#else
-uint16_t Serial_readBytes();
-#endif
 
 #endif /* INC_RINGBUFFERK_H_ */
